4be414971609524f57153baf6c191f4d0da3f0a5
Llausa <santiago.chamon@gmail.com>
2017-01-12 10:36:00 +1000

    Setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  resources :contacts, only: [:new, :create]
   get '/contact', to: 'static_pages#contact'
   get '/projects', to: 'static_pages#projects'
 end
