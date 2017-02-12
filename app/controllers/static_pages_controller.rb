f0b7e7be52bb1ae0ee3e4b20c22997ab78b36bd5
Llausa <santiago.chamon@gmail.com>
2017-02-12 13:55:17 +1000

    About page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def projects
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   resources :contacts, only: [:new, :create]
-  get '/contact', to: 'static_pages#contact'
-  get '/projects', to: 'static_pages#projects'
+  get '/contact', 	to: 'static_pages#contact'
+  get '/projects',  to: 'static_pages#projects'
+  get '/about',		to: 'static_pages#about'
 
   get '*path' => redirect('/')
 end
